14323dbacb261ab10359cab164ebb181f63049be
MAEV-David <87597436+MAEV-David@users.noreply.github.com>
2023-03-02 11:52:37 +0800

    Initial Commit

---
+++ Lab2_5/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Lab2_5
+//
+//  Created by 陳啟瑋 on 2023/3/2.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
